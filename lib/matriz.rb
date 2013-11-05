5e8ece5489c2ff96f91ae5496899102aebf9a2b8
Victor Hernandez <alu0100697032@ull.edu.es>
2013-11-05 17:19:43 +0000

    Añadido el initialize de la matriz para resolver la espectativa

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,2 +1,15 @@
 class matriz
+
+	attr_accessor :fil, :col, :matriz #Getters & Setters
+        
+        def initialize(f,c) #Inicializa el valor de las filas y columnas 
+                raise ArgumentError, 'Tamaño de la matriz no valido' unless f.is_a? Integer and c.is_a? Integer and f > 0 and c > 0                
+                @fil, @col = f, c
+                @matriz = Array.new(@fil,0)
+                i = 0
+                while i < @fil
+                        @matriz[i] = Array.new(@col,0)
+                        i += 1        
+                end
+        end
 end
