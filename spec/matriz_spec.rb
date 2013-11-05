2f217760413cd07cd3eeb1f110acbb96528134c2
Manuel Perez <alu0100697698@ull.edu.es>
2013-11-05 17:52:05 +0000

    Arreglados errores menores

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,13 +1,13 @@
-require "matriz.rb"
+require "lib/matriz.rb"
 
 describe Matriz do
 	
 	before :each do
-		@m1 = Matriz.new(2, 2);
+		@m1 = Matriz.new(2,2);
 		@m1[0] = [1,1]
 		@m1[1] = [1,1]
 		
-		@m2 = Matriz.new(2, 2)
+		@m2 = Matriz.new(2,2)
 		@m2[0] = [2,2]
 		@m2[1] = [2,2]
 		
@@ -25,5 +25,41 @@ describe Matriz do
 		it "# Almacenar el numero de columnas" do
 			@m1.col
 		end
+		
+		it "# Se puede acceder a los elementos de la matriz"
+			@m1[1] = [1,1]
+			@m1[1][0].should eq(1)
+		end
+		
+		it "# Se puede almacenar en la matriz" do
+			@m1[0] = [4,5]
+			@m1[1] = [6,7]
+			@m1[0][1].should eq(5)
+			@m1[1][1].should eq(7)
+		end
 	end
+	
+	describe "# Operadores aritméticos" do
+		it "# Suma de matrices" do
+			@resultado=@m1+@m2
+			@resultado[0][0].should eq(3)
+			@resultado[1][0].should eq(3)
+		end
+		
+		it "# Resta de matrices" do
+			@resultado=@m2+@m1
+			@resultado[0][0].should eq(1)
+			@resultado[1][0].should eq(1)
+		end
+		
+	end
+	
+	describe "# Operador unario" do
+		it "# Operador negacion multiplicando por -1" do
+			@negacion=(-@m1)
+			@negacion[0][1].should eq(-1)
+		end
+	end
+	
+	
 end
